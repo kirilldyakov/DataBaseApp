b2a7a6d69060501aadf434dc86376b584b11e522
Kirill Dyakov <Dyakovks@gmail.com>
2018-08-05 22:55:43 +0300

    Добавляется информация о заправках в Firebase

---
+++ app/src/main/java/ru/strongit/database/Fillup.java
@@ -0,0 +1,11 @@
+package ru.strongit.database;
+
+import java.util.Date;
+
+public class Fillup {
+    Date date;
+    Float volume;
+    Float price;
+    Float summa;
+    Float odometr;
+}
--- app/src/main/java/ru/strongit/database/MainActivity.java
+++ app/src/main/java/ru/strongit/database/MainActivity.java
@@ -1,30 +1,53 @@
 package ru.strongit.database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
-
-    private String TAG="TAGX";
+    private Button btnLogout;
+    private String TAG = "TAGX";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI() {
+        btnLogout = findViewById(R.id.btnLogOut);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+
     }
 
     @Override
@@ -34,10 +57,34 @@ public class MainActivity extends AppCompatActivity {
         //db();
 
         auth();
+
+        db2();
+    }
+
+    private void db2() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("users/"+FirebaseAuth.getInstance().getUid());
+
+        DatabaseReference myRef = ref.child("fillups");
+
+        Fillup fillup = new Fillup();
+        fillup.date=new Date();
+        fillup.odometr = Float.valueOf(250000);
+        fillup.price = 45.67f;
+        fillup.summa = 456.7f;
+        fillup.volume = 10f;
+
+        List<Fillup> fillups = new ArrayList<>();
+        fillups.add(fillup);
+        fillups.add(fillup);
+
+        myRef.setValue(fillups);
+
     }
 
     private void db() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         DatabaseReference myRef = database.getReference("message1");
 
 
@@ -66,18 +113,40 @@ public class MainActivity extends AppCompatActivity {
 // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build()
-                ,new AuthUI.IdpConfig.PhoneBuilder().build()
-                ,new AuthUI.IdpConfig.GoogleBuilder().build()
-                ,new AuthUI.IdpConfig.TwitterBuilder().build()
-//                ,new AuthUI.IdpConfig.FacebookBuilder().build()
+                , new AuthUI.IdpConfig.PhoneBuilder().build()
+                , new AuthUI.IdpConfig.GoogleBuilder().build()
+//                ,new AuthUI.IdpConfig.TwitterBuilder().build()
+                //,new AuthUI.IdpConfig.FacebookBuilder().build()
         );
 
 // Create and launch sign-in intent
         startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers).build()
+
+                , RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Log.d(TAG, "onActivityResult: " + bundle.toString());
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: " + user.toString());
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
